048981143d4518f98cfb5607f04c23ef84340ccb
izcheung <icheung24@my.bcit.ca>
2026-01-18 02:26:39 -0800

    chore: delete sign out button from profile page

--- src/features/Profile.tsx
+++ src/features/Profile.tsx
@@ -1,12 +1,8 @@
-import { authService } from '../services/authService';
 const Profile = () => {
     return (
         <div>
             <h1>Profile</h1>
-            <button onClick={() => {
-                authService.logout()
-                alert("Signed out successfully");
-            }}>Sign Out</button>
+        
         </div>
     );
 };
