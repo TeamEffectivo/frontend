aecfbd139098a443e4e1be7c8a95da2c8bb1e1e0
elenajou <elenajou.ej@gmail.com>
2026-01-18 05:25:15 -0800

    bushes

---
+++ src/components/Bush.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Palmo.css';
+import image1 from '../../public/bush1.png';
+import image2 from '../../public/bush2.png';
+
+const Bush = ({ speed = 1 }) => {
+  return (
+    <div className="bush-pixel-container">
+      {/* Background Image */}
+      <img src={image1} alt="frame-1" className="base-pixel" />
+      
+      {/* Top Image that snaps in and out */}
+      <img 
+        src={image2} 
+        alt="frame-2" 
+        className="top-pixel" 
+        style={{ animationDuration: `${speed}s` }}
+      />
+    </div>
+  );
+};
+
+export default Bush;
--- src/components/Palmo.tsx
+++ src/components/Palmo.tsx
@@ -7,13 +7,14 @@ const Palmo = ({ speed = 1 }) => {
   return (
     <div className="pixel-container">
       {/* Background Image */}
-      <img src={image1} alt="frame-1" className="base-pixel" />
+      <img src={image1} alt="frame-1" width="80px" height="120px" className="base-pixel" />
       
       {/* Top Image that snaps in and out */}
       <img 
         src={image2} 
         alt="frame-2" 
         className="top-pixel" 
+        width="80px" height="120px" 
         style={{ animationDuration: `${speed}s` }}
       />
     </div>
